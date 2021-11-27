0212fa51d05af06f4ed22ac897cdc4b0a0d3f6da
Easy2Digital <77144056+easy2digital@users.noreply.github.com>
2021-11-27 17:59:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ def StoreUrl():
 	keyword = str(request.form['kw_input'])
 
 	chrome_options = webdriver.ChromeOptions()
-	chrome_options.add_argument('lang=zh_CN.UTF-8')
+# 	chrome_options.add_argument('lang=zh_CN.UTF-8')
 	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 	chrome_options.add_argument("--headless")
 	chrome_options.add_argument("--disable-dev-shm-usage")
 	chrome_options.add_argument("--no-sandbox")
-	chrome_options.add_experimental_option('excludeSwitches',['enable-automation'])
-	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+# 	chrome_options.add_experimental_option('excludeSwitches',['enable-automation'])
+# 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 	for x in range (1,3):
